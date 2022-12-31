3a4905584aeb9ae3039b79c89a8840d14e1c0249
huangdaxing3 <1332119354@qq.com>
2022-12-31 11:43:42 +0800

    interrupt

--- kernel/interrupt.h
+++ kernel/interrupt.h
@@ -2,7 +2,22 @@
 #define __KERNEL_INTERRUPT_H
 #include "stdint.h"
 typedef void* intr_handler;
+
+//中断门描述符结构体
+struct gate_desc;
+
+extern uint32_t syscall_handler(void);
+
+//初始化中断描述符
+void idt_desc_init(void);
+//初始化可编程中断控制器
+void pic_init(void);
+
+//完成有关中断的所有初始化工作
 void idt_init(void);
+void general_intr_handler(uint8_t vec_nr);
+void exception_init(void);
+void register_handler(uint8_t vector_no, intr_handler function);
 
 /* 定义中断的两种状态: 
  * INTR_OFF 值为 0，表示关中断
