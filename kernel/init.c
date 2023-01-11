84eec48418426f0ebec004e76ae13ad72eee8fb8
huangdaxing3 <1332119354@qq.com>
2023-01-11 13:47:50 +0800

    kernel

--- kernel/init.c
+++ kernel/init.c
@@ -3,9 +3,18 @@
 #include "interrupt.h"
 #include "../device/timer.h"
 #include "memory.h"
+#include "../thread/thread.h"
+#include "../device/console.h"
+#include "../device/keyboard.h"
+#include "../userprog/tss.h"
+
 void init_all() {
     put_str("init_all\n");
     idt_init();     // 初始化中断
+    mem_init();     // 初始化内存管理系统
+    thread_init();  // 初始化线程相关结构
     timer_init();   // 初始化 PIT
-    mem_init();
+    console_init(); //控制台初始化最好放在开中断之前
+    keyboard_init();  //新增
+    tss_init();
 }
